ca07844fe05e60871fde7d7589ceb512d4831699
frostpig <1397233369@qq.com>
2017-02-17 10:53:47 +0800

    Foo的第一次提交

---
+++ src/com/lss/me/F.java
@@ -0,0 +1,27 @@
+package com.lss.me;
+
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.FieldPosition;
+import java.text.ParsePosition;
+
+public class F {
+   public static void main(String[] args) {
+	System.out.println("sfsd");
+	DateFormat date =new DateFormat() {
+		
+		@Override
+		public java.util.Date parse(String source, ParsePosition pos) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		@Override
+		public StringBuffer format(java.util.Date date, StringBuffer toAppendTo,
+				FieldPosition fieldPosition) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	};
+}
+}
